7b21a7f9f50843716daf24716973bd5d8bd3cda1
Rish46h <tyagirishabh121@gmail.com>
2024-02-23 21:45:04 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/Components/Navbar/Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { ShopContext } from '../../Context/ShopContext'
+// import cart_icon from '../Assets/cart_icon.png'
+// import { ShopContext } from '../../Context/ShopContext'
 
 const Navbar = () => {
 
   let [menu,setMenu] = useState("shop");
-  const {getTotalCartItems} = useContext(ShopContext);
+  // const {getTotalCartItems} = useContext(ShopContext);
 
   return (
     <div className='nav'>
@@ -22,13 +22,13 @@ const Navbar = () => {
         <li onClick={()=>{setMenu("dresses")}}><Link to='/dresses' style={{ textDecoration: 'none', color: 'white' }}>Dresses</Link>{menu==="dresses"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("makeovers")}}><Link to='/makeovers' style={{ textDecoration: 'none', color: 'white' }}>Makeovers</Link>{menu==="makeovers"?<hr/>:<></>}</li>
       </ul>
-      <div className="nav-login-cart">
+      {/* <div className="nav-login-cart">
         {localStorage.getItem('auth-token')
         ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace("/");}}>Logout</button>
         :<Link to='/login' style={{ textDecoration: 'none' }}><button>Login</button></Link>}
         <Link to="/cart"><img src={cart_icon} alt="cart"/></Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
-      </div>
+      </div> */}
     </div>
   )
 }
